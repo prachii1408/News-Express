35a07e86c3fa694ef74f8072db12e874dc590639
Prachi Singh <p_bsingh@me.iitr.ac.in>
2023-03-19 13:32:36 +0530

    last commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,13 +5,13 @@ const Home = ({category,heading}) => {
   const [news,setNews]=useState([]);
   
   let api=`https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Frssfeeds${category}.cms`;
-  console.log(api);
+  
   const fetchApiData=async(url)=>{
    try {
        const res=await fetch(url);
        const data=await res.json();
        setNews(data.items);
-       console.log(news);
+       
    } catch (error) {
        console.log(error);
    }
@@ -19,18 +19,20 @@ const Home = ({category,heading}) => {
   fetchApiData(api);
 
    function show(element){
-    return<>
-       <div className="home">
-      <h2 >{element.title}</h2>
-      <img src={element.enclosure.link} alt="news" />
-      <br />
-      <div><h5 className='right' >{element.author}</h5>
-      <h5 className='left'>{element.pubDate}</h5></div>
-      <p className='clear'>{element.description}</p>
-      <button><a href={element.link}>Read more</a></button>
-      <hr className='clear'></hr>
-    </div>
-    </>
+    return(
+       <div className="home" key={element.link}>
+            <h2 >{element.title}</h2>
+            <img src={element.enclosure.link} alt="news" />
+            <br />
+            <div>
+            <h5 className='right' >{element.author}</h5>
+            <h5 className='left'>{element.pubDate}</h5>
+            </div>
+          <p className='clear'>{element.description}</p>
+          <button><a href={element.link}>Read more</a></button>
+          <hr className='clear'></hr>
+      </div>
+    );
    
    }
   
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,23 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
-  
+  const [state,setState]=useState(false);
+  function togglebar(){
+      setState(!state);
+  }
    
   return (
     <div className="nav">
     <input type="checkbox" id="check" />
     <label htmlFor="check" className='checkbtn'>
-    <i className="fa-sharp  fa-solid fa-bars"></i>
+    <i className="fa-sharp  fa-solid fa-bars" onClick={togglebar}></i>
     </label>
     <img src="newsexpress.png" alt="News logo" className='logo' />
-    <ul>
-        <li><Link to="/" >HOME</Link></li>
-        <li><Link to="/sports">SPORTS</Link></li>
-        <li><Link to="/science">SCIENCE</Link></li>
-        <li><Link to="/environment">ENVIRONMENT</Link></li>
-        <li><Link to="/technology">TECHNOLOGY</Link></li>
-        <li><Link to="/entertainment">ENTERTAINMENT</Link></li>
+    <ul style={{left:state?"-100%":"0"}}>
+        <li><Link to="/" onClick={togglebar}>HOME</Link></li>
+        <li><Link to="/sports" onClick={togglebar}>SPORTS</Link></li>
+        <li><Link to="/science" onClick={togglebar}>SCIENCE</Link></li>
+        <li><Link to="/environment" onClick={togglebar}>ENVIRONMENT</Link></li>
+        <li><Link to="/technology" onClick={togglebar}>TECHNOLOGY</Link></li>
+        <li><Link to="/entertainment" onClick={togglebar}>ENTERTAINMENT</Link></li>
     </ul>
     </div>
   )
